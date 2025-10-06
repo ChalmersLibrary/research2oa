bd009a606363c9ecb4cf9e6a570c89bca1dffbcc
Urban Andersson <urban.andersson@chalmers.se>
2025-10-06 14:30:35 +0200

    Fixes and code refinement

--- research2oa.py
+++ research2oa.py
@@ -9,9 +9,7 @@ from datetime import datetime
 import os
 
 # Script for matching Chalmers CRIS publication records with Open Alex contents
-# Will try to match (exact) using DOI and/or PMID first, then fuzzy match using title and (CRIS) publication year
-# Output: CRIS Publication ID, CRIS Title, CRIS Year, CRIS DOI, CRIS PMID, Open Alex Publication ID, OA Title, OA Chalmers Affiliation (0/1), OA Reference Count, Match Type (DOI/PMID/Title/NO MATCH)
-# Use .env file for configuration (see env_example)
+# See README for usage instructions and details
 
 # Variables
 checked = 0
